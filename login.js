bbfbe70ae50f63a7f2ee38f9dc072d2e521fd383
dipcb05 <dipchakraborty71@gmail.com>
2021-01-12 06:41:46 +0600

    1

--- login.js
+++ login.js
@@ -1,17 +1,79 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'nodelogin'
+});
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+var app = express();
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
-    if (username === "admin" && password === "1234") {
-        alert("You have successfully logged in.");
-        location.assign("credentials.html");
-    } else {
-        loginErrorMsg.style.opacity = 1;
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) {
+    //    connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+           // if (results.length > 0) {
+           if(username === 'admin' && password === 'abcd'){
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }
+            response.end();
+        //});
     }
-})
+     else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
+});
+
+
+app.post('/stream', function(request, response) {
+    const username = request.body.username;
+    const password = request.body.password;
+    const ip_address = request.body.ip_address;
+    if (username && password && ip_address) {
+        Stream = require('node-rtsp-stream');
+        stream = new Stream({
+            streamUrl: 'rtsp://' + username + ':' + password + '@' + ip_address + ':554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif',
+            wsPort: 9999
+        });
+        response.sendFile(path.join(__dirname + 'index.html'));
+        response.end();
+    }
+    else {
+        response.send('Please enter IP address, Username and Password properly!');
+        response.end();
+    }
+});
+
+
+app.get('/home', function(request, response) {
+    if (request.session.loggedin) {
+        response.sendFile(path.join(__dirname + 'test.html'));
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
+
+app.listen(3000);
